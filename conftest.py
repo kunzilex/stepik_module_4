8f719062ca40c177fef8f56824d2f7c2187eb8dd
kunzilex <zikish@ukr.net>
2022-07-06 22:00:58 -0400

    new tests with right code are added

--- conftest.py
+++ conftest.py
@@ -1,52 +1,26 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as chrome_options
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name',
                      action='store',
-                     default=None,
+                     default="chrome",
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language',
+                     action='store',
+                     default="en")
+
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': "language"})
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    user_language = request.config.getoption("language")
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-
-@pytest.fixture
-def get_chrome_options():
-    options = chrome_options
-    options.add_argument('chrome')
-    options.add_argument('--start-maximized')
-    options.add_argument('--window-size=800,600')
-    return options
-
-
-@pytest.fixture
-def get_webdriver(get_chrome_options):
-    options = get_chrome_options
-    driver = webdriver.Chrome(options=options)
-    return driver
-
-
-@pytest.fixture
-def setup(request, get_webdriver):
-    driver = get_webdriver
-
-
-
-    # usr->local->bin
